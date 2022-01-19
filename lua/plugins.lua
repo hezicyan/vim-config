cdc59aafc95e6b33cfac0eca408bef52930128a7
HeziCyan <hezicyan@gmail.com>
2022-01-20 00:36:06 +0800

    feat: Update the Tree-sitter configuration.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -68,10 +68,28 @@ return require('packer').startup(function()
     run = ':TSUpdate'
   }
   require('nvim-treesitter.configs').setup{
+    highlight = {
+      enable = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<CR>',
+        node_incremental = '<CR>',
+        node_decremental = '<BS>',
+        scope_incremental = '<TAB>',
+      },
+    },
+    indent = {
+      enable = true,
+    },
     matchup = {
       enable = true,
     },
   }
+  vim.wo.foldmethod = 'expr'
+  vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+  vim.wo.foldlevel = 3
 
   use 'lukas-reineke/indent-blankline.nvim'
   require('indent_blankline').setup{
