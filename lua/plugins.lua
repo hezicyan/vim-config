e172e0ed190244a6e81cda83d966f26ad025da28
HeziCyan <hezicyan@gmail.com>
2022-04-02 23:09:33 +0800

    feat: Add keymappings for Luasaga.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -76,6 +76,70 @@ return require('packer').startup(function()
   }
 
   use 'tami5/lspsaga.nvim'
+  local vim_map = vim.api.nvim_buf_set_keymap
+  vim_map(
+    0,
+    'n',
+    'gr',
+    '<cmd>Lspsaga rename<cr>',
+    { silent = true, noremap = true }
+  )
+  vim_map(
+    0,
+    'n',
+    'gx',
+    '<cmd>Lspsaga code_action<cr>',
+    { silent = true, noremap = true }
+  )
+  vim_map(
+    0,
+    'x',
+    'gx',
+    ':<c-u>Lspsaga range_code_action<cr>',
+    { silent = true, noremap = true }
+  )
+  vim_map(
+    0,
+    'n',
+    'K',
+    '<cmd>Lspsaga hover_doc<cr>',
+    { silent = true, noremap = true }
+  )
+  vim_map(
+    0,
+    'n',
+    'go',
+    '<cmd>Lspsaga show_line_diagnostics<cr>',
+    { silent = true, noremap = true }
+  )
+  vim_map(
+    0,
+    'n',
+    'gj',
+    '<cmd>Lspsaga diagnostic_jump_next<cr>',
+    { silent = true, noremap = true }
+  )
+  vim_map(
+    0,
+    'n',
+    'gk',
+    '<cmd>Lspsaga diagnostic_jump_prev<cr>',
+    { silent = true, noremap = true }
+  )
+  vim_map(
+    0,
+    'n',
+    '<C-u>',
+    "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1, '<c-u>')<cr>",
+    {}
+  )
+  vim_map(
+    0,
+    'n',
+    '<C-d>',
+    "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1, '<c-d>')<cr>",
+    {}
+  )
 
   use {
     'nvim-telescope/telescope.nvim',
